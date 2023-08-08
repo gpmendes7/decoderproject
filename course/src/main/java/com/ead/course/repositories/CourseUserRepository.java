fb1d8c7e79fbd803c4e57829f326f5e5dcdd2687
gpmendes7 <andrewsseven@gmail.com>
2023-08-08 12:00:11 -0300

    source code

--- course/src/main/java/com/ead/course/repositories/CourseUserRepository.java
+++ course/src/main/java/com/ead/course/repositories/CourseUserRepository.java
@@ -7,4 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
 public interface CourseUserRepository extends JpaRepository<CourseUserModel, UUID> {
+
+    boolean existsByCourseAndUserId(CourseModel courseModel, UUID userId);
 }
